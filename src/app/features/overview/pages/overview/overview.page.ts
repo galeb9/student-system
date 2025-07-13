0d7759dab7b941d729dfc3f8b2fc0eff505e0bd3
Matej Grimšič <matej.grimsic@hudlajf.com>
2025-07-13 22:01:05 +0200

    Overview table improved

--- src/app/features/overview/pages/overview/overview.page.ts
+++ src/app/features/overview/pages/overview/overview.page.ts
@@ -8,6 +8,10 @@ import { TableModule } from 'primeng/table';
 import { ConfirmDialog } from 'primeng/confirmdialog';
 import { ToastModule } from 'primeng/toast';
 import { BadgeModule } from 'primeng/badge';
+import { CardModule } from 'primeng/card';
+import { ChipModule } from 'primeng/chip';
+import { TooltipModule } from 'primeng/tooltip';
+
 import { ConfirmationService, MessageService } from 'primeng/api';
 
 import { StudentService } from '../../../../core/services/student.service';
@@ -23,6 +27,9 @@ import { Student } from '../../../../core/models/student.model';
     BadgeModule,
     ConfirmDialog,
     ToastModule,
+    CardModule,
+    ChipModule,
+    TooltipModule,
   ],
   templateUrl: './overview.page.html',
   styleUrl: './overview.page.scss',
@@ -30,11 +37,19 @@ import { Student } from '../../../../core/models/student.model';
   standalone: true,
 })
 export class OverviewPage {
+  readonly MAX_VISIBLE_COURSES = 2;
   students = signal<Student[]>([]);
-  selectedStudent: Student | null = null;
-  displayDialog = signal(false);
+  selectedStudents = signal<Student[]>([]);
 
+  displayDialog = signal(false);
+  first: number = 0;
+  rows: number = 20;
   studentsCount = computed(() => this.students().length);
+  deleteSelectedLabel = computed(() =>
+    this.selectedStudents().length
+      ? `Delete Selected (${this.selectedStudents().length})`
+      : 'Delete Selected'
+  );
 
   constructor(
     private confirmationService: ConfirmationService,
@@ -82,6 +97,7 @@ export class OverviewPage {
 
       accept: () => {
         this.studentService.delete(id);
+        // change this to filter out?
         this.students.set(this.studentService.getAll());
         this.messageService.add({
           severity: 'info',
@@ -98,4 +114,78 @@ export class OverviewPage {
       },
     });
   }
+  confirmBulkDelete(event: Event) {
+    const selected = this.selectedStudents();
+    console.log(selected);
+    if (!selected.length) return;
+
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: `Delete ${selected.length} student(s)?`,
+      header: 'Confirm Bulk Delete',
+      icon: 'pi pi-exclamation-triangle',
+      acceptButtonProps: {
+        label: 'Delete',
+        severity: 'danger',
+      },
+      rejectButtonProps: {
+        label: 'Cancel',
+        severity: 'secondary',
+        outlined: true,
+      },
+      accept: () => {
+        selected.forEach((student) => {
+          this.studentService.delete(student.id);
+        });
+
+        this.students.set(this.studentService.getAll());
+        this.selectedStudents.set([]);
+
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Deleted',
+          detail: `${selected.length} student(s) deleted.`,
+        });
+      },
+      reject: () => {
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Cancelled',
+          detail: 'Bulk deletion cancelled.',
+        });
+      },
+    });
+  }
+
+  onRowSelect(event: any) {
+    console.log('clicked');
+    const selected = event.data; // the selected row object
+    console.log('Selected row:', selected);
+  }
+
+  // next() {
+  //   this.first = this.first + this.rows;
+  // }
+
+  // prev() {
+  //   this.first = this.first - this.rows;
+  // }
+
+  // reset() {
+  //   this.first = 0;
+  // }
+  // pageChange(event: any) {
+  //   this.first = event.first;
+  //   this.rows = event.rows;
+  // }
+
+  // isLastPage(): boolean {
+  //   return this.students()
+  //     ? this.first + this.rows >= this.students().length
+  //     : true;
+  // }
+
+  // isFirstPage(): boolean {
+  //   return this.students().length ? this.first === 0 : true;
+  // }
 }
